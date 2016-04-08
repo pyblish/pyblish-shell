6d9d3955a2630f486e4e309d3f32ccbb0bcfcb6b
Marcus Ottosson <marcus@abstractfactory.io>
2016-04-08 17:21:02 +0000

    Updating .travis.yml

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "Evaluating OS.."
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+    echo "Building in OSX"
     brew update
     brew install qt55 python
     brew link --force qt55
     sip=/usr/local/Cellar/python/2.7.11/Frameworks/Python.framework/Versions/2.7/bin/sip
 
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
+    echo "Building in Linux"
     sudo apt-add-repository -y ppa:beineri/opt-qt551-trusty
     sudo apt-get update
     sudo apt-get install -y qt-latest
@@ -17,9 +20,11 @@ else
     exit 1
 fi
 
+echo "Fetching data.."
 wget http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.tar.gz
 wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
 
+echo "Building sip.."
 tar xvzf sip-4.17.tar.gz
 cd sip-4.17
 python configure.py
@@ -27,9 +32,12 @@ make
 sudo make install
 cd ..
 
+echo "Building PyQt5.."
 tar xvzf PyQt-gpl-5.5.1.tar.gz
 cd PyQt-gpl-5.5.1
 python configure.py --sip=$sip --confirm-license
 make
 sudo make install
 cd ..
+
+echo "Finished install.sh"
