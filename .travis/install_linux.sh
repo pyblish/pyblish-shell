9a65454eff0c9b2da8b593753f27ff0e4eb01904
Marcus Ottosson <marcus@abstractfactory.io>
2016-04-10 09:43:05 +0000

    Update .travis.yml

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -4,8 +4,8 @@ sudo apt-add-repository -y ppa:beineri/opt-qt551
 sudo apt-get update
 sudo apt-get install -y qt-latest
 
-echo source /opt/qt55/bin/qt55-env.sh >> POST_COMMAND
+source /opt/qt55/bin/qt55-env.sh >> POST_COMMAND
 
-echo export QT_ROOT=/opt/qt55 >> POST_COMMAND
-echo export SIP=/usr/bin/sip >> POST_COMMAND
+export QT_ROOT=/opt/qt55
+export SIP=/usr/bin/sip
 
--- .travis/install_osx.sh
+++ .travis/install_osx.sh
@@ -4,5 +4,5 @@ brew update
 brew install qt55 python
 brew link --force qt55
 
-echo export QT_ROOT=/usr/local/opt/qt55 >> POST_COMMAND
-echo export SIP=/usr/local/Cellar/python/2.7.11/Frameworks/Python.framework/Versions/2.7/bin/sip >> POST_COMMAND
+export QT_ROOT=/usr/local/opt/qt55
+export SIP=/usr/local/Cellar/python/2.7.11/Frameworks/Python.framework/Versions/2.7/bin/sip
