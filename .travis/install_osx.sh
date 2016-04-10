c0d96160b3ed781f6a9e6637de56a989e04194c4
Marcus Ottosson <marcus@abstractfactory.io>
2016-04-10 15:30:29 +0000

    Update .travis.yml

--- .travis/install_osx.sh
+++ .travis/install_osx.sh
@@ -8,11 +8,8 @@ brew link --force qt55
 python --version
 echo "Python dir: $(python -c 'import sys;print(sys.executable)')"
 
-export QT_ROOT=/usr/local/Cellar/qt55
+export QT_ROOT=/usr/local/qt55
 export SIP=/System/Library/Frameworks/Python.framework/Versions/2.7/bin/sip
 
-echo "Qt install: $QT_ROOT"
-tree $QT_ROOT
-
-echo "Qt Cellar: /usr/local/Cellar/qt55/"
-tree /usr/local/Cellar/qt55/
+echo "Listing: $QT_ROOT"
+tree %QT_ROOT
