237da3aba33c5233e6578f041ea26d27fb0daae8
Marcus Ottosson <marcus@abstractfactory.io>
2016-04-21 20:50:59 +0000

    Exclude QtPrintSupport due to runtime errors

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,17 +9,20 @@ wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
 echo "Building sip.."
 tar xvzf sip-4.17.tar.gz
 cd sip-4.17
-python configure.py
-make
-sudo make install
+python configure.py && \
+    make && \
+    sudo make install
 cd ..
 
 echo "Building PyQt5.."
 tar xvzf PyQt-gpl-5.5.1.tar.gz
 cd PyQt-gpl-5.5.1
-python configure.py --sip=$SIP --confirm-license
-make
-sudo make install
+python configure.py \
+    --sip=$SIP \
+    --disable=QtPrintSupport \
+    --confirm-license && \
+    make && \
+    sudo make install
 cd ..
 
 echo "Finished install.sh"
