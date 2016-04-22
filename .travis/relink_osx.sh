92949ad50ebc8b62dd86998612f5bd857b3874d0
Marcus Ottosson <marcus@abstractfactory.io>
2016-04-22 12:12:04 +0000

    Repair OSX build

--- .travis/relink_osx.sh
+++ .travis/relink_osx.sh
@@ -6,9 +6,24 @@
 
 sudo chmod u+w build/QtGui && \
     install_name_tool -change "$QT_ROOT/lib/QtWidgets.framework/Versions/5/QtWidgets"   "./QtWidgets"   build/PyQt5.QtWidgets.so && \
-    install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui"           "./QtGui"       build/PyQt5.QtWidgets.so && \
-    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtWidgets.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui"           "./QtGui"       build/PyQt5.QtWidgets.so && \ 
+    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtWidgets.so && \ 
     install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtCore.so && \
     install_name_tool -change "$QT_ROOT/lib/QtWidgets.framework/Versions/5/QtWidgets"   "./QtWidgets"   build/PyQt5.QtGui.so && \
     install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui"           "./QtGui"       build/PyQt5.QtGui.so && \
-    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtGui.so || exit 1
+    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtGui.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtQuick.framework/Versions/5/QtQuick"       "./QtQuick"     build/PyQt5.QtQuick.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui"           "./QtGui"       build/PyQt5.QtQuick.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtQuick.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtQml.framework/Versions/5/QtQml"           "./QtQml"       build/PyQt5.QtQuick.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtNetwork.framework/Versions/5/QtNetwork"   "./QtNetwork"   build/PyQt5.QtQuick.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtNetwork.framework/Versions/5/QtNetwork"   "./QtNetwork"   build/PyQt5.QtNetwork.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtNetwork.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui"           "./QtGui"       build/PyQt5.QtQml.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtQml.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtQml.framework/Versions/5/QtQml"           "./QtQml"       build/PyQt5.QtQml.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtNetwork.framework/Versions/5/QtNetwork"   "./QtNetwork"   build/PyQt5.QtQml.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtWidgets.framework/Versions/5/QtWidgets"   "./QtWidgets"   build/PyQt5.QtTest.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui"           "./QtGui"       build/PyQt5.QtTest.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtTest.framework/Versions/5/QtTest"         "./QtTest"      build/PyQt5.QtTest.so && \
+    echo "Relinking successful" || exit 1
