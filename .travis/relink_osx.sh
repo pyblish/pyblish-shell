ae53788320e2438989736f03f6364e519fe44768
Marcus Ottosson <marcus@abstractfactory.io>
2016-04-22 10:39:03 +0000

    Repair OSX build

--- .travis/relink_osx.sh
+++ .travis/relink_osx.sh
@@ -4,7 +4,7 @@
 # It shouldn't cause trouble if at some point cx_Freeze
 # does the right thing.
 
-sudo chmod u+w QtGui && \
+sudo chmod u+w build/QtGui && \
     install_name_tool -change "$QT_ROOT/lib/QtWidgets.framework/Versions/5/QtWidgets"   "./QtWidgets"   build/PyQt5.QtWidgets.so && \
     install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui"           "./QtGui"       build/PyQt5.QtWidgets.so && \
     install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtWidgets.so && \
