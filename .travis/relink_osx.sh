1ce35f50294df165f3da60eec13bda3cdaba8639
Marcus Ottosson <marcus@abstractfactory.io>
2016-04-22 09:47:23 +0000

    Make relink_osx.sh exit with error when failed

--- .travis/relink_osx.sh
+++ .travis/relink_osx.sh
@@ -4,15 +4,11 @@
 # It shouldn't cause trouble if at some point cx_Freeze
 # does the right thing.
 
-# QtGui is write protected by default.
-sudo chmod u+w QtGui
-
-# Relink paths that normally point, absolutely,
-# to the locally installed version of Qt.
-install_name_tool -change "$QT_ROOT/lib/QtWidgets.framework/Versions/5/QtWidgets" "./QtWidgets" build/PyQt5.QtWidgets.so
-install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui" "./QtGui" build/PyQt5.QtWidgets.so 
-install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore" "./QtCore" build/PyQt5.QtWidgets.so 
-install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore" "./QtCore" build/PyQt5.QtCore.so
-install_name_tool -change "$QT_ROOT/lib/QtWidgets.framework/Versions/5/QtWidgets" "./QtWidgets" build/PyQt5.QtGui.so
-install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui" "./QtGui" build/PyQt5.QtGui.so
-install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore" "./QtCore" build/PyQt5.QtGui.so
+sudo chmod u+w QtGui && \
+    install_name_tool -change "$QT_ROOT/lib/QtWidgets.framework/Versions/5/QtWidgets"   "./QtWidgets"   build/PyQt5.QtWidgets.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui"           "./QtGui"       build/PyQt5.QtWidgets.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtWidgets.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtCore.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtWidgets.framework/Versions/5/QtWidgets"   "./QtWidgets"   build/PyQt5.QtGui.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtGui.framework/Versions/5/QtGui"           "./QtGui"       build/PyQt5.QtGui.so && \
+    install_name_tool -change "$QT_ROOT/lib/QtCore.framework/Versions/5/QtCore"         "./QtCore"      build/PyQt5.QtGui.so || exit 1
